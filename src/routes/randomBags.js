b7c8f01551be7643abf4a38a60a750f445b0c8e0
Gustavo Rivera <griverab@unal.edu.co>
2025-02-22 00:12:19 -0500

    reserve random bag

--- src/routes/randomBags.js
+++ src/routes/randomBags.js
@@ -2,28 +2,75 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models/db");
 
+//reservar producto
+router.post("/randomBags/reserve", async (req, res) => {
+  try {
+    //get params
+    const { user_id, random_bag_id } = req.body;
+    const result0 = await db.query(
+      "select * from orders where random_bag_id = $1",
+      [random_bag_id]
+    );
+    if (result0.rows.length > 0) {
+      return res.status(400).json({ error: "La bolsa ya fue reservada" });
+    }
+    //query create order
+    const result = await db.query(
+      "insert into orders (user_id, random_bag_id, current_status) values ($1, $2, 'reserved') RETURNING *",
+      [user_id, random_bag_id]
+    );
+    console.log("orden creada: ", result.rows);
+    if (result.rows.length > 0) {
+      //query update random_bag
+      const result2 = await db.query(
+        "update random_bag set available = false where random_bag_id = $1 RETURNING *",
+        [random_bag_id]
+      );
+      console.log("random_bag actualizada: ", result2.rows);
+      res.json(result2.rows[0]);
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 router.post("/randomBags", async (req, res) => {
   try {
-    const { store_id, description, total_price, discount_price, pick_up_time, available } = req.body;
+    const {
+      store_id,
+      description,
+      total_price,
+      discount_price,
+      pick_up_time,
+      available,
+    } = req.body;
 
     // Validaciones adicionales
     if (!store_id) {
       return res.status(400).json({ error: "store_id es requerido" });
     }
     if (isNaN(total_price) || isNaN(discount_price)) {
-      return res.status(400).json({ error: "total_price y discount_price deben ser números válidos" });
+      return res.status(400).json({
+        error: "total_price y discount_price deben ser números válidos",
+      });
     }
 
     const result = await db.query(
       `INSERT INTO random_bag (store_id, description, total_price, discount_price, pick_up_time, available) 
        VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
-      [store_id, description, total_price, discount_price, pick_up_time, available]
+      [
+        store_id,
+        description,
+        total_price,
+        discount_price,
+        pick_up_time,
+        available,
+      ]
     );
     console.log("Datos obtenidos:", result.rows); // Log para depuración
     res.json(result.rows[0]);
   } catch (err) {
-    if (err.code === '23505') { 
+    if (err.code === "23505") {
       res.status(400).json({ error: "El store_id ya existe" });
     } else {
       res.status(500).json({ error: err.message });
@@ -33,7 +80,9 @@ router.post("/randomBags", async (req, res) => {
 
 router.get("/randomBags", async (req, res) => {
   try {
-    const result = await db.query("SELECT * FROM random_bag");
+    const result = await db.query(
+      "SELECT * FROM random_bag where available = true order by created_at desc"
+    );
     res.json(result.rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
