f751acdfa60d0ea459ae58405430f720dd340d22
Gustavo Rivera <griverab@unal.edu.co>
2025-02-24 01:04:39 -0500

    var entorno

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const randomBagsRouter = require("./routes/randomBags");
 const ordersRouter = require("./routes/orders");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -11,7 +14,8 @@ app.use(express.json());
 app.use("/api/store", randomBagsRouter);
 app.use("/api/store", ordersRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+// console.log(PORT);
 app.listen(PORT, () => {
   console.log("Backend corriendo en puerto", PORT);
 });
